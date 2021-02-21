c8032a1328e88f917320ac158d1341abbe3f0916
ecolon1 <ecolon@oriontek.do>
2021-02-21 15:57:17 -0400

    cleaning code

--- CashRegister.js
+++ CashRegister.js
@@ -1,19 +1,14 @@
 function checkCashRegister(price, cash, cid) {
   
     let cidCopy = cid.map(x => x.slice());
-    
     const coinsPrice  = [0.01,0.05,0.1,0.25,1,5,10,20,100];
     const coinsName = ["PENNY","NICKEL","DIME","QUARTER","ONE","FIVE","TEN","TWENTY","ONE HUNDRED"]
     let devuelta = cash - price;
     let result = [];
+    
     let maxIndex = getMaxIndex(devuelta,coinsPrice);
-    
-    
-  
     let changeAmount = getChangeAmount(devuelta,cidCopy,coinsName,maxIndex,result);
     
-    console.log(cidCopy)
-  
     if(changeAmount.length === 0){
       return {status:"INSUFFICIENT_FUNDS",change:[]}
     }else if(isCIDClosed(cidCopy)){
@@ -23,11 +18,11 @@ function checkCashRegister(price, cash, cid) {
     }
     
   }
-  //bien
+  
   const getMaxIndex = (devuelta,coinsPrice) => {
     return coinsPrice.filter(x => x <= devuelta).length - 1;
   }
-  //bien
+  
   const isCIDClosed = (cid) => {
     let result = 0;
     for(let i = 0; i < cid.length;i++){
@@ -39,31 +34,22 @@ function checkCashRegister(price, cash, cid) {
   const getChangeAmount = (devuelta,cid,coinsName,maxIndex,result) => {
     switch(maxIndex){
       case 0:
-      //(devuelta,cid,maxIndex,result,coinValue)
       return workCase(devuelta,cid,maxIndex,result,coinsName,1);
       case 1:
-      //(devuelta,cid,maxIndex,result,coinValue)
       return workCase(devuelta,cid,maxIndex,result,coinsName,5);
       case 2:
-      //(devuelta,cid,maxIndex,result,coinValue)
       return workCase(devuelta,cid,maxIndex,result,coinsName,10);
       case 3:
-      //(devuelta,cid,maxIndex,result,coinValue)
       return workCase(devuelta,cid,maxIndex,result,coinsName,25);
       case 4:
-      //(devuelta,cid,maxIndex,result,coinValue)
       return workCase(devuelta,cid,maxIndex,result,coinsName,100);
       case 5:
-      //(devuelta,cid,maxIndex,result,coinValue)
       return workCase(devuelta,cid,maxIndex,result,coinsName,500);
       case 6:
-      //(devuelta,cid,maxIndex,result,coinValue)
       return workCase(devuelta,cid,maxIndex,result,coinsName,1000)
       case 7:
-      //(devuelta,cid,maxIndex,result,coinValue)
       return workCase(devuelta,cid,maxIndex,result,coinsName,2000)
       case 8:
-      //(devuelta,cid,maxIndex,result,coinValue)
       return workCase(devuelta,cid,maxIndex,result,coinsName,10000)
       case -1:
       return [];
@@ -71,26 +57,20 @@ function checkCashRegister(price, cash, cid) {
   }
   
   const workCase = (devuelta,cid,maxIndex,result,coinsName,coinValue) => {
-  //cantidad de monedas disponibles en el CID
+  //CID Available Amount of Coins
       let cidAmountOfCoins = getCidAmountOfCoins(cid,maxIndex,coinValue);
-      //cantidad de monedas necesarias para la devuelta
+      //Amount of coins needed to charge back
       let devueltaAmountOfCoins = getDevueltaAmountOfCoins(devuelta,coinValue);
-      //si hay monedas en el CID para devolver
-    
+      //if there is an amount available of coins
       if(devueltaAmountOfCoins.coinAmount <= cidAmountOfCoins){
-        
-        
         let coins = devueltaAmountOfCoins.coinAmount*devueltaAmountOfCoins.coin;
         if(coins > 0)
           result.push([coinsName[maxIndex],coins]);
   
-        
-  
         cid[maxIndex][1] =cid[maxIndex][1] - coins;
         devuelta = Number.parseFloat(devuelta - coins).toFixed(2);
-          
       }else{
-        //la devuelta es mayor que lo que tengo en caja
+        //In case that there amount to chargeback is more than the one in the CID
         let coins = cidAmountOfCoins*devueltaAmountOfCoins.coin;
         result.push([coinsName[maxIndex],coins])
         const valueInCID = cid[maxIndex][1];
@@ -121,7 +101,8 @@ function checkCashRegister(price, cash, cid) {
     return {coinAmount:cant,coin:coinValue/100,excedente:devueltaEnEnteros/100}
   }
   
-  const getCidAmountOfCoins = (cid,maxIndex,coinValue) =>{
+  const getCidAmountOfCoins = (cid,maxIndex,coinValue) =>
+  {
   return cid[maxIndex][1]*100/coinValue;
   }
   
